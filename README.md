ff7b32664d43c7e8a182e4f973d6388c25c6d46d
Jaakko Salonen <jaakko.salonen@iki.fi>
2016-10-09 02:03:53 +0300

    feat: Add support for custom docker hostname and port with SWARMIST_DOCKER_URI

--- README.md
+++ README.md
@@ -2,12 +2,16 @@
 
 Experimental GUI and tools for Docker Swarm Mode
 
+## Running Locally
+
+	docker run -it -v /var/run/docker.sock:/var/run/docker.sock -p 3000:3000 swarmist
+
 ## Running Against Remote Swarm
 
 Create an SSH tunnel to manager node:
 
 	ssh -NL localhost:2375:/var/run/docker.sock user@example.com
 
-Start swarmist:
+Start swarmist and connect to tunneled port:
 
-	npm start
+	SWARMIST_DOCKER_URI=http://localhost:2375 npm start
--- server/index.js
+++ server/index.js
@@ -1,5 +1,14 @@
 var Docker = require('dockerode');
-var docker = new Docker({host: '127.0.0.1', port: 2375});
+var url = require('url');
+var dockerOpts = undefined;
+if(process.env.SWARMIST_DOCKER_URI) {
+  var url = url.parse(process.env.SWARMIST_DOCKER_URI);
+  dockerOpts = {
+    host: url.hostname || '127.0.0.1',
+    port: url.port || 2375
+  };
+}
+var docker = new Docker(dockerOpts);
 var express = require('express');
 var app = express();
 
@@ -8,47 +17,48 @@ app.get('/api', (req, res) => {
 });
 
 app.get('/api/containers', (req, res) => {
-	docker.listContainers((err, containers) => {
-	  if(err) {
-	    return res.status(500).send(err);
-	  } else {
-			return res.json(containers);
-	  }
-	});
+  docker.listContainers((err, containers) => {
+    if(err) {
+      return res.status(500).send(err);
+    } else {
+      return res.json(containers);
+    }
+  });
 });
 
 app.get('/api/services', (req, res) => {
-	docker.listServices((err, services) => {
-	  if(err) {
-	    return res.status(500).send(err);
-	  } else {
-			return res.json(services);
-	  }
-	});
+  docker.listServices((err, services) => {
+    if(err) {
+      return res.status(500).send(err);
+    } else {
+      return res.json(services);
+    }
+  });
 });
 
 app.get('/api/services/:id', (req, res) => {
-	var service = docker.getService(req.params.id);
-	service.inspect((err, inspect) => {
-	  if(err) {
-	    return res.status(500).send(err);
-	  } else {
-			return res.json(inspect);
-	  }
-	});
+  var service = docker.getService(req.params.id);
+  service.inspect((err, inspect) => {
+    if(err) {
+      return res.status(500).send(err);
+    } else {
+      return res.json(inspect);
+    }
+  });
 });
 
 
 app.get('/api/tasks', (req, res) => {
-	docker.listTasks((err, tasks) => {
-	  if(err) {
-	    return res.status(500).send(err);
-	  } else {
-		return res.json(tasks);
-	  }
-	});
+  docker.listTasks((err, tasks) => {
+    if(err) {
+      return res.status(500).send(err);
+    } else {
+    return res.json(tasks);
+    }
+  });
 });
 
 app.listen(3001, function () {
-	console.log('Server running');
+  console.log('Docker connection options:', dockerOpts);
+  console.log('Server running');
 });

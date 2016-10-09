154f163eb4ebcfc0749a1d4d0f18feec00bdbcfe
Jaakko Salonen <jaakko.salonen@iki.fi>
2016-10-10 01:06:46 +0300

    feat: Visualize replica status using symbols

--- client/src/ServiceReplicaStatus.js
+++ client/src/ServiceReplicaStatus.js
@@ -2,19 +2,35 @@ import React, { Component } from 'react';
 
 class ServiceReplicaStatus extends Component {
   render() {
-    let scalingStatus = '';
-    if(this.props.replicas > this.props.replicasRunning) {
-      scalingStatus = ' (scaling up \u2191)';
+    const running = this.props.replicasRunning;
+    const desired = this.props.replicas;
+    let symbol = '?';
+    let  color = 'black';
+
+    if(desired === 0 && running === 0) {
+      symbol = '\u2716';
+      color = 'red';
+    } else if(desired === running) {
+      symbol = '\u2714';
+      color = 'green';
+    } else if(desired > running && running === 0) {
+      symbol = '\u25B2';
+      color = 'red';
+    } else if(desired > running) {
+      symbol = '\u25B2';
+      color = 'green';
+    } else if(desired < running) {
+      symbol = '\u25BC';
+      color = 'orange';
     }
-    if(this.props.replicas < this.props.replicasRunning) {
-      scalingStatus = ' (scaling down \u2193)';
-    }
+
     return (
       <span>
-        {this.props.replicasRunning}
-        &nbsp; / &nbsp;
-        {this.props.replicas}
-        {scalingStatus}
+        <span style={{color: color}}>{symbol}</span>        
+        {' '}
+        {running}
+        {' / '} 
+        {desired}
       </span>
     );
   }

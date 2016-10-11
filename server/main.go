f1dbc4d9ddeedfed4128c3221fe7cff34b2a1d77
Jaakko Salonen <jaakko.salonen@iki.fi>
2016-10-12 00:08:52 +0300

    refactor: Initial backend rewrite using golang

---
+++ server/main.go
@@ -0,0 +1,77 @@
+package main
+
+import (
+  "fmt"
+  "context"
+
+  "github.com/kataras/iris"
+  "github.com/docker/docker/api/types"
+  "github.com/docker/docker/api/types/swarm"
+  "github.com/docker/docker/client"
+)
+
+type SwarmistService struct{
+  swarm.Service
+  ReplicasRunning int
+}
+
+func main(){
+  cli, err := client.NewEnvClient()
+  if err != nil {
+    panic(err)
+  }
+
+  iris.Get("/", func(ctx *iris.Context) {
+    requestpath := "client/build/index.html"
+    ctx.ServeFile(requestpath, true)
+  })
+
+  iris.Get("/static/*file", func(ctx *iris.Context) {
+    requestpath := "client/build/static" + ctx.Param("file")
+    ctx.ServeFile(requestpath, true)
+  })
+
+  iris.Get("/api/services", func(ctx *iris.Context){
+    services, err := cli.ServiceList(context.Background(), types.ServiceListOptions{})
+    if err != nil {
+      fmt.Println(err)
+      ctx.EmitError(iris.StatusInternalServerError)
+      return
+    }
+
+    tasks, err := cli.TaskList(context.Background(), types.TaskListOptions{})
+    if err != nil {
+      fmt.Println(err)
+      ctx.EmitError(iris.StatusInternalServerError)
+      return
+    }
+
+    swarmistServices := make([]SwarmistService, len(services))
+    for i,service := range services {
+      swarmistService := SwarmistService{service, 0}
+
+      replicasRunning := 0
+      for _,task := range tasks {
+        if (task.ServiceID == service.ID && task.Status.State == "running") {
+          replicasRunning++
+        }
+      }
+      swarmistService.ReplicasRunning = replicasRunning
+      swarmistServices[i] = swarmistService
+    }
+
+    ctx.JSON(iris.StatusOK, swarmistServices)
+  })
+
+  iris.Get("/api/tasks", func(ctx *iris.Context){
+    tasks, err := cli.TaskList(context.Background(), types.TaskListOptions{})
+    if err != nil {
+      ctx.EmitError(iris.StatusInternalServerError)
+      return
+    }
+
+    ctx.JSON(iris.StatusOK, tasks)
+  })
+
+  iris.Listen(":3001")
+}
